04c2e16b43792d53cb3b80d5f7a76ea1dde29732
netsi <netsi@users.noreply.github.com>
2026-03-02 14:29:14 +0100

    stream: parallelize producer_retry_consumer;

--- pkg/stream/retry_sqs.go
+++ pkg/stream/retry_sqs.go
@@ -30,8 +30,8 @@ type RetryHandlerSqsSettings struct {
 	RunnerCount         int                        `cfg:"runner_count" default:"1"`
 	QueueId             string                     `cfg:"queue_id"`
 	Healthcheck         health.HealthCheckSettings `cfg:"healthcheck"`
-	MsgBodyEncoding     string `cfg:"msg_body_encoding" default:"raw"`
-	Unmarshaller        string `cfg:"unmarshaller" default:"msg"`
+	MsgBodyEncoding     string                     `cfg:"msg_body_encoding" default:"raw"`
+	Unmarshaller        string                     `cfg:"unmarshaller" default:"msg"`
 }
 
 type RetryHandlerSqs struct {
@@ -47,7 +47,10 @@ func NewRetryHandlerSqs(ctx context.Context, config cfg.Config, logger log.Logge
 		settings = &RetryHandlerSqsSettings{}
 	)
 
-	config.UnmarshalKey(md.retryConfigKey, settings)
+	err = config.UnmarshalKey(md.retryConfigKey, settings)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to unmarshal retry handler settings for %q: %w", md.name, err)
+	}
 
 	if settings.QueueId == "" {
 		settings.QueueId = fmt.Sprintf("consumer-retry-%s", md.name)
